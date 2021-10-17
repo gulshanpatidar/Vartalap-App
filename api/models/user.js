6f8e3270dff405470d81cd2539a3deb67b3598c0
tanishgupta-git <tanishgupta8181@gmail.com>
2021-10-17 11:08:49 +0530

    added authentication

---
+++ api/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+     type:String,
+     require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
---
+++ api/routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { body } = require('express-validator/check');
+const User = require('../models/user');
+const router = express.Router();
+const authController = require('../controllers/auth');
+
+
+router.put('/signup',[
+    body('email').isEmail().withMessage("Please enter a valid email").bail().custom((value,{ req }) => {
+        return User.findOne({email : value}).then(userDoc => {
+            if (userDoc) {
+                return Promise.reject('E-mail address already exists!');
+            }
+        })
+    }).normalizeEmail(),
+    body('password').trim().isLength({min : 5}),
+    body('username').trim().isLength({min : 5}).bail().custom((value,{ req }) => {
+        return User.findOne({username : value}).then(userDoc => {
+            if (userDoc) {
+                return Promise.reject('username already taken!');
+            }
+        })
+    })
+],authController.userSignup);
+
+
+router.post('/login',authController.userLogin);
+
+
+module.exports = router;
